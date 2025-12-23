cc2dd0a0068139a3b900dbb40f4ee4fb2589dae6
TirnakHastaligiAnalizSistemi <meldanur.sener9003@gmail.com>
2025-12-23 09:03:03 +0300

    İkili sistem.py

--- ikili sistem.py
+++ ikili sistem.py
@@ -10,7 +10,6 @@ from sklearn.metrics import (
     classification_report,
     accuracy_score
 )
-
 # Modeli yüklüyoruz
 binary_model_path = "best_densenet121binary.keras"
 multiclass_model_path = "best_densenet121.keras"
@@ -66,11 +65,6 @@ for i in range(len(data_gen)):
     y_binary_decision.append(1)
 print("Sistem Tamamlandı")
 
-# Modellerin Kaydedilmesi
-binary_model.save("binary_model_final.keras")
-multiclass_model.save("multiclass_model_final.keras")
-print(" Modeller kaydedildi.")
-
 # Karışıklık Matrisi
 valid_idx = [i for i in range(len(y_pred)) if y_pred[i] != -1]
 y_true_f = [y_true[i] for i in valid_idx]
@@ -90,9 +84,13 @@ plt.tight_layout()
 plt.show()
 multiclass_acc = accuracy_score(y_true_f, y_pred_f)
 print(f"İkili Sistem 6'lı Sınıflandırmadan Cıkan Dogruluk: %{multiclass_acc*100:.2f}")
-
+# Modellerin Kaydedilmesi
+binary_model.save("binary_model_final.keras")
+multiclass_model.save("multiclass_model_final.keras")
+print(" Modeller kaydedildi.")
 # İkili Sistemin Dogrulugunu Buluyoruz
 binary_true = [1 if y != -1 else 0 for y in y_true]
 binary_pred = y_binary_decision
 binary_acc = accuracy_score(binary_true, binary_pred)
 print(f"\nİkili Sistem Dogrulugu: %{binary_acc*100:.2f}")
+
